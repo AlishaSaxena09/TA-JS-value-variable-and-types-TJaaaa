9fb7e82afd68470f63a74b37f1bbd25acb9e21e8
Alisha Saxena <alishasaxena2016@gmail.com>
2021-01-11 15:48:43 +0530

    revision

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,39 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for(let i = 0; i< 10; i++ ){
+    console.log(i)
+}
 // Using `console.log` log all the evan values from 1 to 10.
+
+for(let i = 0; i< 10; i++ ){
+    if(i%2 === 0) {
+        console.log(i)
+    }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
 
+for(let i = 0; i< 10; i++ ){
+    if(i%2 != 0) {
+        console.log(i)
+    }
+}
+
 // Calculate the sum of all numbers from 1 to 10.
 
+var total = 0;
+
+    for(var i = 1; i <= 10; i++){
+      total += i;
+    }
+
+
 // Log all the values from 1 to 10 using while loop
+
+let i = 0;
+
+while (i < 10) {
+    console.log(i);
+    i = i + 1;
+}
